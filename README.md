7e1e27538d8998db43a43af08a1b77f4e6fb928d
Ashwaq Alharbi <ashwaq.alharbi.19@hotmail.com>
2022-06-15 16:35:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# JavaScript-Fundamentals-HW
+
+## Variables
+- Calculate area of a rectangle
+    - Store the length of rectangle. 
+    - Store the width of rectangle.
+    - Calculate the area and print the output "The area of the rectangle is: NN" 
+    
+- The Temperature Converter  
+    - Store a celsius temperature into a variable.
+    - Convert it to fahrenheit and output "NN°C is NN°F".
+    - Now store a fahrenheit temperature into a variable.
+    - Convert it to celsius and output "NN°F is NN°C."
+
+## For loop
+### Numbers' summations
+* Loop though this array `let Numbers = [23,54,32,87,47]` and print the sum of all numbers
+
+### Maximum number
+- Create an array of the following numbers [16,4,2,0,19,6]
+- Loop through the array elements and print the maximum number of this list to the console.
+
+### Reverse Array 
+- Create an array of the following numbers [1,2,3,4,5,6,7,8,9,10]
+- print the array in a reverse order
+
+
+## Nested for loop
+
+
+### stars pattern
+- Declare a variable and assign a number to it.
+- Print a right angle triangle using stars to the console. Example: if the variable is 5, the printed pattern will be as follow:
+
+\* <br>
+\** <br>
+\*** <br>
+\**** <br>
+\***** <br>
+
---
+++ index.js
@@ -0,0 +1,124 @@
+//JavaScript Homework
+
+
+// Calculate area of a rectangle
+
+// Store the length of rectangle.
+// Store the width of rectangle.
+// Calculate the area and print the output "The area of the rectangle is: NN"
+// The Temperature Converter
+// -------------------------------------------------------------------
+
+function getRectangleArea(length, width) {
+    return length*width;
+  }
+
+  let area = getRectangleArea(4,5);
+
+console.log("The area of your rectangle is " + area);
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+//The Temperature Converter
+
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
+// -------------------------------------------------------------------
+
+function celsiusToFahrenheit(celsius) {
+  var celsiusInF = (celsius * 9) / 5 + 32;
+  console.log(celsius + "°C is " + celsiusInF + "°F");
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  var fahrenheitInC = ((fahrenheit - 32) * 5) / 9;
+  console.log(fahrenheit + "°F is " + fahrenheitInC + "°C");
+}
+
+
+celsiusToFahrenheit(60);
+fahrenheitToCelsius(45);
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+// Numbers' summations
+// Loop though this array let Numbers = [23,54,32,87,47] and print the sum of all numbers
+// -------------------------------------------------------------------
+
+var Numbers = [23,54,32,87,47];
+for (var i = 0, sum = 0; i < Numbers.length; sum += Numbers[i++]);
+
+console.log(sum)
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+//another way
+
+const Numbers = [23,54,32,87,47];
+let sum = 0;
+
+for (let i = 0; i < Numbers.length; i++) {
+    sum += Numbers[i];
+}
+console.log(sum);
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+// Maximum number
+// Create an array of the following numbers [16,4,2,0,19,6]
+// Loop through the array elements and print the maximum number of this list to the console.
+// -------------------------------------------------------------------
+
+var numbers = [16,4,2,0,19,6];
+var max = numbers[0];
+for (var i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i];
+  }
+}
+console.log("Max is: " + max);
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+// Reverse Array
+// Create an array of the following numbers [1,2,3,4,5,6,7,8,9,10]
+// print the array in a reverse order
+
+// -------------------------------------------------------------------
+
+var arrayNumbers = [1,2,3,4,5,6,7,8,9,10];
+var revNumber = arrayNumbers.reverse();
+console.log("revNumber", revNumber);
+
+// ------------------------------------------------------------------- 
+// ------------------------------------------------------------------- 
+
+
+// stars pattern
+// Declare a variable and assign a number to it.
+// Print a right angle triangle using stars to the console. 
+// ------------------------------------------------------------------- 
+
+
+let n = 10;
+
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 0; j < n - i; j++) {
+    process.stdout.write(' ');
+  }
+  // printing star
+  for (let k = 0; k < i; k++) {
+    process.stdout.write('*');
+  }
+  console.log();
+}
+// ------------------------------------------------------------------- 
+// ------------------------------------------------------------------- 
